173a3000adc5578b78d78b21df08d4c1f2a9dd0e
GoogleMapsTest <Anton@DESKTOP-1GNI1S9>
2018-10-28 17:21:35 +0100

    Invoke javaScript Works.
    - Get current userlocation does not work

---
+++ TestGoogleMapsWPF/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using CefSharp;
+
+
+namespace TestGoogleMapsWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           // loadFile();
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+            var script = string.Format("addMarker({0}, {1});", LatitudeTextBox.Text, LongtitudeTextBox.Text);
+            ChromiumWebBrowser.EvaluateScriptAsync(script);
+          
+
+        }
+
+
+
+
+       
+    }
+}
